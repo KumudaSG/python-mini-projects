24f449d65102c3dff79850221cabdc983b899c31
Kumuda S <ksg6@illinois.edu>
2024-01-16 23:02:34 -0600

    changed

--- Webdev/Basic task app/app.py
+++ Webdev/Basic task app/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+#app is an instance of Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
+db = SQLAlchemy(app)
+
+#registering a route to the home page
 @app.route('/')
+
 def index():
     return render_template('index.html')
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(host = '0.0.0.0',debug = True)
